72dff793190e1347263686b5af736eb331f6aace
adit <aditya_hermansyahputra@yahoo.com>
2019-06-21 13:27:47 +0700

    calculator

---
+++ app/src/main/java/id/cashflow/android/util/Calculator.kt
@@ -0,0 +1,10 @@
+package id.cashflow.android.util
+
+class Calculator {
+    fun sum(x: Int, y: Int): Int {
+        return x + y
+    }
+    fun sub(x: Int, y: Int): Int = x - y
+    fun times(x: Int, y: Int): Int = x * y
+    fun div(x: Int, y: Int): Int = x / y
+}
