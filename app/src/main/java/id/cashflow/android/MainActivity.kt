16df358c10d2b436c4fb5935cd1bbb6f39ca8692
adit <aditya_hermansyahputra@yahoo.com>
2019-06-22 08:38:05 +0700

    user interface

--- app/src/main/java/id/cashflow/android/MainActivity.kt
+++ app/src/main/java/id/cashflow/android/MainActivity.kt
@@ -1,14 +1,23 @@
 package id.cashflow.android
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import id.cashflow.android.util.Calculator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ivNotification.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Notification",Toast.LENGTH_SHORT).show()
+        }
+        ivSetting.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Setting",Toast.LENGTH_SHORT).show()
+        }
 
         println("HALO ANDROID !")
 
